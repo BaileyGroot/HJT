061153435b89a8ec44194a82cad24dd652c4fe6a
Markus Bailey Stockfleth Thomsbæk Groot <mark4805@zbc.dk>
2021-12-09 00:31:36 +0100

    Add project files.

---
+++ HJT/Message.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HJT
+{
+    class Message
+    {
+
+        public enum MessageCarrier { Smtp, VMessage }
+
+        string to, from, body, subject, cc;
+
+        public Message(string to, string from, string body, string subject, string cc)
+        {
+            this.to = to;
+            this.from = from;
+            this.body = body;
+            this.subject = subject;
+            this.cc = cc;
+        }
+
+        public string To { get => to; set => to = value; }
+        public string From { get => from; set => from = value; }
+        public string Body { get => body; set => body = value; }
+        public string Subject { get => subject; set => subject = value; }
+        public string Cc { get => cc; set => cc = value; }
+
+
+
+        public string ConvertBodyToHTML(string plainText)
+        {
+            return "" + plainText + "";
+        }
+
+        public void sendMessageToAll(MessageCarrier type, string[] to, Message m, bool isHTML)
+        {
+            if (type.Equals(MessageCarrier.Smtp))
+            {
+                if (isHTML)
+                    m.Body = ConvertBodyToHTML(m.Body);
+                //her implementeres alt koden til at sende via Smtp
+            }
+
+            if (type.Equals(MessageCarrier.VMessage))
+            {
+                if (isHTML)
+                    m.Body = ConvertBodyToHTML(m.Body);
+                //her implementeres alt koden til at sende via VMessage
+            }
+        }
+    }
+}
+}
---
+++ HJT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HJT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
